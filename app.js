67c0bbc24fa0cdcdbf5e9bc5b147554780eee5f8
Enstso <enstso@outlook.fr>
2024-12-10 16:57:11 +0100

    logger and update swagger

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const amqp = require("amqplib");
 const app = express();
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const logger = require("./logger");
 
 // Configuration Swagger
 const swaggerOptions = {
@@ -56,7 +57,7 @@ const Book = sequelize.define(
 // Initialisation de la base de données
 (async () => {
   await sequelize.sync();
-  console.log("Base de données synchronisée");
+  logger.info("Base de données synchronisée");
 })();
 
 // RabbitMQ
@@ -66,9 +67,9 @@ const rabbitConnection = async () => {
     const connection = await amqp.connect("amqp://admin:admin@rabbitmq");
     rabbitChannel = await connection.createChannel();
     await rabbitChannel.assertQueue("availability_queue", { durable: true });
-    console.log("Connecté à RabbitMQ");
+    logger.info("Connecté à RabbitMQ");
   } catch (error) {
-    console.error("Erreur de connexion à RabbitMQ:", error);
+    logger.error("Erreur de connexion à RabbitMQ:", error);
   }
 };
 
@@ -77,33 +78,45 @@ const rabbitConnection = async () => {
 /**
  * @swagger
  * /v1/books:
- *   post:
- *     summary: Ajouter un nouveau livre
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - title
- *               - author
- *             properties:
- *               title:
- *                 type: string
- *               author:
- *                 type: string
- *               published_year:
- *                 type: integer
- *               isbn:
- *                 type: string
- *               availability:
- *                 type: boolean
+ *   get:
+ *     summary: Récupérer les livres
+ *     parameters:
+ *       - in: query
+ *         name: title
+ *         required: false
+ *         schema:
+ *           type: string
+ *         description: Titre du livre
+ *       - in: query
+ *         name: author
+ *         required: false
+ *         schema:
+ *           type: string
+ *         description: Auteur du livre
  *     responses:
- *       201:
- *         description: Livre ajouté avec succès
- *       400:
- *         description: Erreur de validation
+ *       200:
+ *         description: Liste des livres récupérée avec succès
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   title:
+ *                     type: string
+ *                   author:
+ *                     type: string
+ *                   published_year:
+ *                     type: integer
+ *                   isbn:
+ *                     type: string
+ *                   availability:
+ *                     type: boolean
+ *       404:
+ *         description: Aucun livre trouvé avec les critères donnés
+ *       500:
+ *         description: Erreur interne du serveur
  */
 
 app.get("/v1/books", async (req, res) => {
@@ -114,11 +127,14 @@ app.get("/v1/books", async (req, res) => {
     if (author) where.author = { [Sequelize.Op.like]: `%${author}%` };
 
     const books = await Book.findAll({ where });
-    if (books.length === 0)
+    if (books.length === 0) {
+      logger.warn("No books found with the given criteria");
       return res.status(404).json({ message: "No books found" });
-
+    }
+    logger.info("Books fetched successfully");
     res.json(books);
   } catch (error) {
+    logger.error(`Failed to fetch books: ${error.message}`);
     res
       .status(500)
       .json({ error: "Failed to fetch books", message: error.message });
@@ -160,15 +176,17 @@ app.get("/v1/books", async (req, res) => {
 app.post("/v1/books", async (req, res) => {
   try {
     const { title, author, published_year, isbn, availability } = req.body;
-    if (!title || !author)
+    if (!title || !author) {
+      logger.warn("Title and author are required");
       return res.status(400).json({ error: "Title and author are required" });
-
+    }
     const existingBook = await Book.findOne({ where: { isbn } });
-    if (existingBook)
+    if (existingBook) {
+      logger.warn("Book with this ISBN already exists");
       return res
         .status(400)
         .json({ error: "Book with this ISBN already exists" });
-
+    }
     const newBook = await Book.create({
       title,
       author,
@@ -176,10 +194,13 @@ app.post("/v1/books", async (req, res) => {
       isbn,
       availability,
     });
+
+    logger.info("Book added successfully");
     res
       .status(201)
       .json({ message: "Book added successfully", book_id: newBook.id });
   } catch (error) {
+    logger.error("Failed to add book");
     res
       .status(500)
       .json({ error: "Failed to add book", message: error.message });
@@ -230,11 +251,15 @@ app.put("/v1/books/:id", async (req, res) => {
     const { title, author, published_year, isbn, availability } = req.body;
 
     const book = await Book.findByPk(id);
-    if (!book) return res.status(404).json({ error: "Book not found" });
-
+    if (!book){ 
+      logger.warn("Book not found")
+      return res.status(404).json({ error: "Book not found" });
+  }
     await book.update({ title, author, published_year, isbn, availability });
+    logger.info(`Book ${id} updated successfully`);
     res.json({ message: "Book updated successfully" });
   } catch (error) {
+    logger.warn("Failed to update book");
     res
       .status(500)
       .json({ error: "Failed to update book", message: error.message });
@@ -267,11 +292,16 @@ app.delete("/v1/books/:id", async (req, res) => {
     const { id } = req.params;
 
     const book = await Book.findByPk(id);
-    if (!book) return res.status(404).json({ error: "Book not found" });
+    if (!book){
+      logger.warn(`Book ${id} not found`);
+      return res.status(404).json({ error: "Book not found" });
+    } 
 
     await book.destroy();
+    logger.info(`Book ${id} deleted successfully`);
     res.json({ message: "Book deleted successfully" });
   } catch (error) {
+    logger.error("Failed to delete book");
     res
       .status(500)
       .json({ error: "Failed to delete book", message: error.message });
@@ -292,7 +322,8 @@ app.delete("/v1/books/:id", async (req, res) => {
  *         description: ID du livre
  *     responses:
  *       200:
- *         description: Disponibilité du livre
+ *         description: Disp
+ * onibilité du livre
  *         content:
  *           application/json:
  *             schema:
@@ -311,9 +342,14 @@ app.get("/v1/books/:id/availability", async (req, res) => {
     const { id } = req.params;
 
     const book = await Book.findByPk(id);
-    if (!book) return res.status(404).json({ error: "Book not found" });
+    if (!book){
+      logger.warn("Book not found");
+      return res.status(404).json({ error: "Book not found" });
+    }
+    logger.info(`the book is ${id} available`);     
     res.json({ availability: book.availability });
   } catch (error) {
+    logger.error(`Failed to check availability of the book ${id}`);
     res
       .status(500)
       .json({ error: "Failed to check availability", message: error.message });
@@ -323,7 +359,7 @@ app.get("/v1/books/:id/availability", async (req, res) => {
 // Consommateur RabbitMQ
 const processMessage = async (msg) => {
   try {
-    const { LivreID,livreId } = JSON.parse(msg.content.toString());
+    const { LivreID, livreId } = JSON.parse(msg.content.toString());
     const book_id = LivreID ? LivreID : livreId;
     const book = await Book.findByPk(book_id);
     if (!book) return console.error(`Book ID ${bookId} not found`);
@@ -332,22 +368,22 @@ const processMessage = async (msg) => {
         return console.error(`Book ID ${book_id} not available`);
       const newAvailability = !book.availability;
       await book.update({ availability: newAvailability });
-    console.log(
-      `Book ID ${book_id} availability updated to ${newAvailability}`
+      logger.info(
+        `Book ID ${book_id} availability updated to ${newAvailability}`
       );
-    } else if(LivreID) {
+    } else if (LivreID) {
       if (!book.availability)
         return console.error(`Book ID ${book_id} is available`);
       const newAvailability = !book.availability;
       await book.update({ availability: newAvailability });
-      console.log(
+      logger.info(
         `Book ID ${book_id} availability updated to ${newAvailability}`
       );
     }
 
     rabbitChannel.ack(msg);
   } catch (error) {
-    console.error("Erreur dans le traitement du message:", error.message);
+    logger.error("Erreur dans le traitement du message:", error.message);
     rabbitChannel.nack(msg);
   }
 };
@@ -356,19 +392,19 @@ const startConsumer = async () => {
   await rabbitConnection();
   rabbitChannel.consume("emprunts_finished_queue", processMessage);
 
-  console.log("En attente des messages dans la file 'availability_queue'...");
+  console.log("En attente des messages dans la file 'emprunts_finished_queue'...");
 };
 
 const startConsumer2 = async () => {
-    await rabbitConnection();
-    rabbitChannel.consume("emprunts_created_queue", processMessage);
-  
-    console.log("En attente des messages dans la file 'availability_queue'...");
-  };
-  
+  await rabbitConnection();
+  rabbitChannel.consume("emprunts_created_queue", processMessage);
+
+  console.log("En attente des messages dans la file 'emprunts_created_queue'...");
+};
 
 // Démarrer l'application
 app.listen(3000, async () => {
+  logger.info("Serveur démarré sur le port 3000");
   console.log("Serveur démarré sur le port 3000");
   await rabbitConnection();
   startConsumer();
